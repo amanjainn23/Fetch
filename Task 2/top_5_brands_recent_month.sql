952535e00c00dc41b30963783baec67fb6050780
Aman Jain <jainamanr@gmail.com>
2024-11-17 23:03:40 -0600

    Second Commit

--- Task 2/top_5_brands_recent_month.sql
+++ Task 2/top_5_brands_recent_month.sql
@@ -4,4 +4,4 @@ JOIN Brands b ON r.brandId = b._id
 WHERE DATE_TRUNC('month', r.dateScanned) = DATE_TRUNC('month', CURRENT_DATE - INTERVAL '1 month')
 GROUP BY b.name
 ORDER BY receipt_count DESC
-LIMIT 5;
+LIMIT 10;
